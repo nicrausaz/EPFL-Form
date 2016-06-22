9a4f3720992df9464c715841ba879c434fc5c2d4
nicrausaz <n.crausaz99@gmail.com>
2016-06-22 15:29:26 +0200

    add main page for tequila login

--- index.php
+++ index.php
@@ -1,270 +1,43 @@
 <!doctype html>
 <html lang="fr">
     <head>  
-         <meta charset="UTF-8"/>
+        <meta charset="UTF-8"/>
          <link rel="stylesheet" type="text/css" href="style.css">
          <link rel="icon" type="image/png" href="img/favicon.png" />
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-         <script>
-             $(document).ready(function(){ 
-                $("#all").hide()
-                $("#lSch1").hide(0)
-                $("#lSch2").hide(0)
-
-                    $("#jb").change(function(){
-                      var sele = $("#jb option:selected").text();
-                      
-                        if((sele=="Polymécanicien-ne CFC")||(sele=="Informaticien-ne CFC")||(sele=="Logisticien-ne CFC")||(sele=="Planificateur-trice éléctricien-ne CFC")||(sele=="Employé-e de commerce CFC")||(sele=="Gardien-ne d'animaux CFC")){
-                            $("#all").show(1000)
-                            $("#infoOnly").hide(0)
-                            
-                            if(sele=="Informaticien-ne CFC"){
-                                $("#infoOnly").show(1000)
-                            }
-
-                        }else if((sele=="Laborantin-e CFC; option biologie")||(sele=="Laborantin-e CFC; option chimie")||(sele=="Laborantin-e en physique CFC")){
-                            $("#all").hide(1000)
-
-                            if(confirm("Pour les métiers de laborantins, l'inscription se fait au près de ASSOCIATION, cliquer sur ok pour être rediriger...")){
-                                window.location.replace("https://google.com");
-                            }else{}
-                        }
-                        });
-
-                       $("#photo").change(function(){
-                        var fileExtension = ['jpeg', 'jpg', 'png', 'gif', 'bmp'];
-                        var input = $("#photo");
-                        if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-                            alert("Format non pris en charge, Formats autorisés : "+fileExtension.join(', '));
-                            }
-                        });
-                        
-                        $("#cv").change(function() {
-                        var fileExtension = ['pdf'];
-                        if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-                            alert("Format non pris en charge, Formats autorisés : "+fileExtension.join(', '));
-                            }
-                        });
-
-                        $("#lettre").change(function() {
-                        var fileExtension = ['pdf'];
-                        if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-                            alert("Format non pris en charge, Formats autorisés : "+fileExtension.join(', '));
-                            }
-                        });
-                        });
-                         
-
-            </script>      
-         <title>Formulaire Apprentissage</title>
+         <title>Accueil</title>
          <meta name="description" content="Formulaire condidature apprentissage EPFL"/>
     </head>
     <body>
-    <div class="form-style-5" >
-        <form method ="post" action="cible.php" enctype="multipart/form-data">
-            
-        <fieldset id="photoTitle">
+    <div class="form-style-5">
+
+    <fieldset id="photoTitle">
             <img src="./img/FA.png"/ alt="FA" width="315" height="150" id="FAPic" draggable="false"/>
             <img src="./img/epfl.png"/ alt="EPFL" width="300" height="150" id="epflPic" draggable="false"/>
             <p><h2>Candidature pour un Apprentissage</h2>
-            Les champs notés d'un astérisque* doivent être obligatoirement remplis.<p>
-        </fieldset> 
-         
-        <fieldset>  
-            <!-- DONNEES APPRENTISSAGE-->
-            <legend><span class="number">1</span> Apprentissage</legend>
-            
-            
-            <label for="job">Je suis intéressé par le métier de*: </label>
-            <select name ="job" id="jb" required>
-                <option value="menu" selected disabled>Choisir un métier...</option>
-                <option value="laboBio">Laborantin-e CFC; option biologie</option>
-                <option value="laboCh">Laborantin-e CFC; option chimie</option>
-                <option value="laboPhy">Laborantin-e en physique CFC</option>
-                <option value="polyM">Polymécanicien-ne CFC</option>
-                <option value="info">Informaticien-ne CFC</option>
-                <option value="logi">Logisticien-ne CFC</option>
-                <option value="planElec">Planificateur-trice éléctricien-ne CFC</option>
-                <option value="empCom">Employé-e de commerce CFC</option>
-                <option value="gardAn">Gardien-ne d'animaux CFC</option>
-            </select>
-           </fieldset>
-           <div id="all">
-           <fieldset>
-           <div id="infoOnly">
-            <label for="filInfo">Filières informatiques:*</label>
+            Veuillez suivre les indication suivantes<p>
+    </fieldset>
 
-            <dl class="radio-list-left" required>
-            <dd>
-                <input type="radio" name="filInfo" id="fill1" value="devIT" >
-                <label for="fill1">Dévellopement d'application</label>
-            </dd>
-            <dd>
-                <input type="radio" name="filInfo" id="fill2" value="entrIT" checked="checked">
-                <label for="fill2">Informatique d'entreprise</label>
-            </dd>
-            <dd>
-                <input type="radio" name="filInfo" id="fill3" value="techIT">
-                <label for="fill3">Technique des systèmes</label>
-            </dd>
-            <dd>
-                <input type="radio" name="filInfo" id="fill4" value="dontKnow">
-                <label for="fill4">Je ne sais pas</label>
-            </dd>
-            </dl>
-            </div>
+    <fieldset>
+        <legend><span class="number">1</span> Créer un compte temporaire </legend>
 
-            <label for="mpt">Je désire m'inscire en maturité professionelle intégrée*:</label><p>
-            <dl class="radio-list-left">
-            <dd>
-                <input type="radio" name="mpt" id="mpt1" value="Matu-non" checked="checked">
-                <label for="mpt1">Non</label>
-            </dd>
-            <dd>
-                <input type="radio" name="mpt" id="mpt2" value="Matu-oui">
-                <label for="mpt2">Oui</label>
-            </dd>
-            </dl>
+        Cliquer sur le bouton créer un compte.</p>
+        Sur la page ouverte, cliquer sur "Créer un compte pour personnes externes"</p>
+        Remplir les champs et confirmer (pour le champ Organisation, entrer "Postulation Apprentis")</p>
+        Attendre la récéption du mail de confirmation puis cliquer sur le lien pour confirmer l'inscription.</p>
 
-            <div>
-            <p>
-          </fieldset>
-          <fieldset>
-            
-            <legend><span class="number">2</span> Données </legend>
-            <fieldset>
-            <legend><span class="number">2.1</span> Données personnelles</legend>    
-                
-               <!-- DONNEES APPRENTIS-->
-               <label for="photo">Photo passeport couleur*:</label>
+        <a href="https://guests.epfl.ch/" id="createAcountB">Créer un compte</a>  
+    </fieldset>
+    <fieldset>
+        <legend><span class="number">2</span> Se connecter et remplir le formulaire </legend>
+        Entrer vos informations de compte temporaire:</p>
+        **login tequila**</p>
+        **login tequila**</p>
+        **login tequila**</p>
+        Lorsque la connexion sera effectuée, vous serez rediriger vers le formulaire
+    </fieldset>
 
-               <label class="file" title=""><input type="file" name="photo" id="photo" onchange="this.parentNode.setAttribute('title', this.value.replace(/^.*[\\/]/, ''))" required/></label>
+    
 
-               <!-- <input type="file" name="photo" id="photo"/><p> -->
-            <select name="genreApp" >
-                <option disabled selected> Choisissez un genre*</option>
-                <option value="Homme" >Homme</option>
-                <option value="Femme" >Femme</option>
-            </select>
-            
-            <input type="text" name="nameApp" placeholder="Nom *" autocomplete="off" required/>
-            <input type="text" name="surnameApp" placeholder="Prénom *" autocomplete="off" required/>
-            <input type="text" name="adrApp" placeholder="Adresse *" autocomplete="off" />
-            <input type="text" name="NPAApp" placeholder="NPA\Domicile *" autocomplete="off" />
-            <input type="tel" name="telApp" placeholder="Téléphone *" autocomplete="off" />
-            <input type="tel" name="phoneApp" placeholder="Mobile *" autocomplete="off" />
-            <input type="email" name="mailApp" placeholder = "Mail *"autocomplete="off" /> 
-            <input type="date" name="birthApp" max="today" />
-            <input type="text" name="originApp" placeholder="Lieu d'origine *" autocomplete="off" />
-            <input type="text" name="nationApp" placeholder="Nationalité *" autocomplete="off" />
-            <input type="text" name="langApp" placeholder="Langue Maternelle *" autocomplete="off" />
-            
-            <label for="languesApp">Connaissance linguistiques*:</label>
-            <p><input type="checkbox" name="connLing" id="french" value="Français" /><label for="french"><span class="ui"></span>Français</label></p>
-            <p><input type="checkbox" name="connLing" id="german" value="Allemand"/><label for="german"><span class="ui"></span>Allemand</label></p>
-            <p><input type="checkbox" name="connLing" id="english" value="Anglais"/><label for="english"><span class="ui"></span>Anglais</label></p>
-            <p><input type="checkbox" name="connLing" id="other" value="Autres"/><label for="other"><span class="ui"></span>Autres</label></p>
-
-        </fieldset>
-        <fieldset>
-            <legend><span class="number">2.2</span> Réprésentants légaux</legend>    
-             (Si vous avez moins de 18 ans.)<p>
-                <!-- DONNEES REPRESENTANT 1-->
-              Réprésentant 1:<p>
-            <select name="genreRep1" >
-                <option disabled selected> Choisissez un genre</option>
-                <option value="Homme">Homme</option>
-                <option value="Femme">Femme</option>
-            </select>
-
-            <input type="text" name="nameRep1" placeholder="Nom" autocomplete="off"/>
-            <input type="text" name="surnameRep1" placeholder="Prénom" autocomplete="off"/>
-             <input type="text" name="adrRep1" placeholder="Adresse" autocomplete="off"/>
-            <input type="text" name="NPARep1" placeholder = "NPA\Domicile"autocomplete="off"/>
-            <input type="text" name="telRep1" placeholder="Téléphone" autocomplete="off"/>
-            
-             Réprésentant 2:<p>
-                <!-- DONNEES REPRESENTANT 2-->
-            <select name="genreRep2" >
-                <option disabled selected> Choisissez un genre</option>
-                <option value="Homme">Homme</option>
-                <option value="Femme">Femme</option>
-            </select>
-            
-            <input type="text" name="nameRep2" placeholder="Nom" autocomplete="off"/>
-            <input type="text" name="surnameRep2" placeholder="Prénom" autocomplete="off"/>
-             <input type="text" name="adrRep2" placeholder="Adresse" autocomplete="off"/>
-            <input type="text" name="NPARep2" placeholder = "NPA\Domicile"autocomplete="off"/>
-            <input type="text" name="telRep2" placeholder="Téléphone" autocomplete="off"/>   
-        </fieldset>
-         <!-- ACIVITES-->
-        <legend><span class="number">3</span> Activités</legend>
-        <fieldset>
-            <legend><span class="number">3.1</span> Scolarité</legend>
-
-                <table id="scolaire">
-                    <tr>
-                        <td><input type="text" name="ecole1" placeholder="Ecole" autocomplete="off"/></td>
-                        <td><input type="text" name="lieu1" placeholder="Lieu" autocomplete="off"/></td>
-                        <td><input type="text" name="niveau1" placeholder="Niveau" autocomplete="off"/></td>
-                        <td><input type="text" name="annees1" placeholder="de-à(années)" autocomplete="off"/></td>  
-                    </tr>
-
-                    <tr id="lSch1">
-                        <td><input type="text" name="ecole2" placeholder="Ecole" autocomplete="off"/></td>
-                        <td><input type="text" name="lieu2" placeholder="Lieu" autocomplete="off"/></td>
-                        <td><input type="text" name="niveau2" placeholder="Niveau" autocomplete="off"/></td>
-                        <td><input type="text" name="annees2" placeholder="de-à(années)" autocomplete="off"/></td>
-                    </tr>
-                     <tr id="lSch2">
-                        <td><input type="text" name="ecole3" placeholder="Ecole" autocomplete="off"/></td>
-                        <td><input type="text" name="lieu3" placeholder="Lieu" autocomplete="off"/></td>
-                        <td><input type="text" name="niveau3" placeholder="Niveau" autocomplete="off"/></td>
-                        <td><input type="text" name="annees3" placeholder="de-à(années)" autocomplete="off"/></td>
-                    </tr>
-                </table>
-                <input type="button" id="addSch" value="Ajouter une ligne" onclick="addFieldSch()">
-        </fieldset>
-        <fieldset>
-            <legend><span class="number">3.2</span> Activités professionelles</legend>
-            <table id="activites">
-                    <tr>
-                        <td><input type="text" name="employeur1" placeholder="Employeur" autocomplete="off"/></td>
-                        <td><input type="text" name="lieu4" placeholder="Lieu" autocomplete="off"/></td>
-                        <td><input type="text" name="activite1" placeholder="Activité" autocomplete="off"/></td>
-                        <td><input type="text" name="annees4" placeholder="de-à(années)" autocomplete="off"/></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="employeur2" placeholder="Employeur" autocomplete="off"/></td>
-                        <td><input type="text" name="lieu5" placeholder="Lieu" autocomplete="off"/></td>
-                        <td><input type="text" name="activite1" placeholder="Activité" autocomplete="off"/></td>
-                        <td><input type="text" name="annees5" placeholder="de-à(années)" autocomplete="off"/></td>
-                    </tr>
-                </table>
-                <input type="button" id="addPro" value="Ajouter une ligne" onclick="addLpro()"/>
-        </fieldset>
-        <fieldset>
-            <legend><span class="number">3.3</span> Stages</legend>
-            test test test test test
-        </fieldset>
-        <!-- ANNEXES-->
-        <fieldset>
-        </fieldset>
-        <legend><span class="number">4</span> Annexes à fournir</legend>
-        <label for="CV">CV*:</label>
-                <label class="file" title=""><input type="file" name="cv" id="cv" onchange="this.parentNode.setAttribute('title', this.value.replace(/^.*[\\/]/, ''))" required/></label>
-                
-        <label for="lettre">Lettre de motivation*:</label>
-                <label class="file" title=""><input type="file" name="lettre" id="lettre" onchange="this.parentNode.setAttribute('title', this.value.replace(/^.*[\\/]/, ''))" required/></label>
-                    
-        </fieldset> 
-        <fieldset>  
-        <p><input type="checkbox" value="conditionsAcc" id="conditions"/><label for="conditions"><span class="ui"></span>Accepter les <a href="conditions.php"> conditions</label></p>
-
-        <input type="submit" value="Terminer">
-        </fieldset>
-        </div>
-        </form>
-        </div>
-    </body>
+    </div>
+</body>
 </html>
