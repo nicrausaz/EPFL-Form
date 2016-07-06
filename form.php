008edc84d1f1f71a5e79d0bf7149ee992e0d8d5d
nicrausaz <n.crausaz99@gmail.com>
2016-07-06 14:11:29 +0200

    replace tequila

--- form.php
+++ form.php
@@ -7,7 +7,7 @@
          <?php
 
             ini_set("display_errors",0);error_reporting(0);
-            require_once("tequila.php");
+            require_once("tequila/tequila.php");
             $oClient = new TequilaClient();
             $oClient->SetApplicationName('Formulaire apprentissage');
             $oClient->SetWantedAttributes(array('uniqueId','firstname','name'));
