c8324f647c5d574b885d5f16cb78f9680644e6f1
nicrausaz <n.crausaz99@gmail.com>
2016-08-25 08:52:03 +0200

    get all infos

--- cible.php
+++ cible.php
@@ -128,6 +128,23 @@
                         $doc->rep1  = array("genre"=>$_POST['genreRep1'],"nom"=>$_POST['nameRep1'],"prenom"=>$_POST['surnameRep1'],"addresse"=> array("rue"=>$_POST['adrRep1'],"NPA"=>$_POST['NPARep1']),"telephone"=>$_POST['telRep1']);    
                         $doc->rep2  = array("genre"=>$_POST['genreRep2'],"nom"=>$_POST['nameRep1'],"prenom"=>$_POST['surnameRep2'],"addresse"=> array("rue"=>$_POST['adrRep2'],"NPA"=>$_POST['NPARep2']),"telephone"=>$_POST['telRep2']);           
                     }else{}
+                    $doc->scolarite1 = array("ecole"=>$_POST['ecole1'],"lieu"=>$_POST['lieuEcole1'],"niveau"=>$_POST['niveauEcole1'],"annees"=>$_POST['anneesEcole1']);
+                    $doc->scolarite2 = array("ecole"=>$_POST['ecole2'],"lieu"=>$_POST['lieuEcole2'],"niveau"=>$_POST['niveauEcole2'],"annees"=>$_POST['anneesEcole2']);
+                    $doc->scolarite3 = array("ecole"=>$_POST['ecole3'],"lieu"=>$_POST['lieuEcole3'],"niveau"=>$_POST['niveauEcole3'],"annees"=>$_POST['anneesEcole3']);
+                    $doc->scolarite4 = array("ecole"=>$_POST['ecole4'],"lieu"=>$_POST['lieuEcole4'],"niveau"=>$_POST['niveauEcole4'],"annees"=>$_POST['anneesEcole4']);
+                    $doc->scolarite5 = array("ecole"=>$_POST['ecole5'],"lieu"=>$_POST['lieuEcole5'],"niveau"=>$_POST['niveauEcole5'],"annees"=>$_POST['anneesEcole5']);
+                    $doc->anneeFinScolarite = $_POST['anneeFin'];
+                    $doc->activiteProfessionelle1 = array("employeur"=>$_POST['employeurPro1'],"lieu"=>$_POST['lieuPro1'],"activite"=>$_POST['activitePro1'],"annees"=>$_POST['anneesPro1']);
+                    $doc->activiteProfessionelle2 = array("employeur"=>$_POST['employeurPro2'],"lieu"=>$_POST['lieuPro2'],"activite"=>$_POST['activitePro2'],"annees"=>$_POST['anneesPro2']);
+                    $doc->activiteProfessionelle3 = array("employeur"=>$_POST['employeurPro3'],"lieu"=>$_POST['lieuPro3'],"activite"=>$_POST['activitePro3'],"annees"=>$_POST['anneesPro3']);
+                    $doc->activiteProfessionelle4 = array("employeur"=>$_POST['employeurPro4'],"lieu"=>$_POST['lieuPro4'],"activite"=>$_POST['activitePro4'],"annees"=>$_POST['anneesPro4']);
+                    $doc->stage1 = array("metier"=>$_POST['activiteStage1'],"employeur"=>$_POST['entrepriseStage1']);
+                    $doc->stage2 = array("metier"=>$_POST['activiteStage2'],"employeur"=>$_POST['entrepriseStage2']);
+                    $doc->stage3 = array("metier"=>$_POST['activiteStage3'],"employeur"=>$_POST['entrepriseStage3']);
+                    $doc->dejaCandidat = $_POST['dejaCand'];
+                    if($_POST['dejaCand'] == "dejaCand-oui"){
+                        $doc->anneeCandidature = $_POST['dejaCandAnnee'];
+                    }else{}
 
                     //$doc->nowDate = new DateTime();
                     
--- json/jsonClass.php
+++ json/jsonClass.php
@@ -29,64 +29,25 @@ class Doc {
 	public $majeur = "";
 	public $rep1 = [];
 	public $rep2 = [];
-	/*
-	public $genreRep1 = "";
-	public $nomRep1 = "";
-	public $prenomRep1 = "";
-	public $AdresseRep1 = "";
-	public $NPARep1 = "";
-	public $telephoneRep1 = "";
-	public $genreRep2 = "";
-	public $nomRep2 = "";
-	public $prenomRep2 = "";
-	public $AdresseRep2 = "";
-	public $NPARep2 = "";
-	public $telephoneRep2 = "";
-	*/
+
 	//3.1
-	public $ecole1 = "";
-	public $lieuEcole1 = "";
-	public $niveauEcole1 = "";
-	public $anneesEcole1 = "";
-	public $ecole2 = "";
-	public $lieuEcole2 = "";
-	public $niveauEcole2 = "";
-	public $anneesEcole2 = "";
-	public $ecole3 = "";
-	public $lieuEcole3 = "";
-	public $niveauEcole3 = "";
-	public $anneesEcole3 = "";
-	public $ecole4 = "";
-	public $lieuEcole4 = "";
-	public $niveauEcole4 = "";
-	public $anneesEcole4 = "";
-	public $ecole5 = "";
-	public $lieuEcole5 = "";
-	public $niveauEcole5 = "";
-	public $anneesEcole5 = "";
-	public $anneeFinEcole = "";
+	public $scolarite1 = [];
+	public $scolarite2 = [];
+	public $scolarite3 = [];
+	public $scolarite4 = [];
+	public $scolarite5 = [];
+	public $anneeFinScolarite = "";
+
 	//3.2
-	public $employeurPro1 = "";
-	public $lieuPro1 = "";
-	public $activitePro1 = "";
-	public $anneesPro1 = "";
-	public $employeurPro2 = "";
-	public $lieuPro2 = "";
-	public $activitePro2 = "";
-	public $anneesPro2 = "";
-	public $employeurPro3 = "";
-	public $lieuPro3 = "";
-	public $activitePro3 = "";
-	public $anneesPro3 = "";
+	public $activiteProfessionelle1 = [];
+	public $activiteProfessionelle2 = [];
+	public $activiteProfessionelle3 = [];
+
 	//3.3
-	public $metierStage1 = "";
-	public $entrepriseStage1 = "";
-	public $metierStage2 = "";
-	public $entrepriseStage2 = "";
-	public $metierStage3 = "";
-	public $entrepriseStage3 = "";
-	public $metierStage4 = "";
-	public $entrepriseStage4 = "";
+	public $stage1 = [];
+	public $stage2 = [];
+	public $stage3 = [];
+	public $stage4 = [];
 	public $dejaCandidat = "";
 	public $anneeCandidature = "";
 	//others
