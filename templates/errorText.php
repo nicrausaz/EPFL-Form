5f2d08aba131ecc03f235bee9e24a9f16519554a
nicrausaz <n.crausaz99@gmail.com>
2017-03-02 12:50:42 +0100

    more and more readable errors

--- templates/errorText.php
+++ templates/errorText.php
@@ -1,8 +1,8 @@
 <p style="color:red; font-size: 25px;">Des erreurs se sont produites, merci de remplir tous les champs obligatoires!</p>
 <div id="formErrorsDiv" style="padding-bottom: 20px;">
-  <?php //print_r($validator->errors());
+  <?php
     foreach($validator->errors() as $error => $errorVal) {
-      echo "<li>$error : $errorVal </li>";
+      echo "<li>$errorVal</li>";
     }
   ?>
 </div>
