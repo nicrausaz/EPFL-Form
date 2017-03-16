3a7690acfd3aa614071f82753cec468f95bec8fc
nicrausaz <n.crausaz99@gmail.com>
2017-03-16 10:42:00 +0100

    update email addresses

--- templates/mailToResp.php
+++ templates/mailToResp.php
@@ -6,7 +6,7 @@ $message = "Candidat: ".$surname." ".$name."\n\n".
 "Apprentissage: ".$job."\n\n".
 "Consulter la candidature sur: "."\\\\scxdata\\apprentis$\\candidatures\\nouvelles";
 
-$headers = 'From: formulaireApprentis@epfl.ch' . "\r\n" .
-'Reply-To: formulaireApprentis@epfl.ch' . "\r\n" .
+$headers = 'From: noreply+formulaireApprentis@epfl.ch' . "\r\n" .
+'Reply-To: formation.apprentis@epfl.ch' . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
 ?>
