7dfebdab6154f990c6cb271d9779d1573024c7f6
nicrausaz <n.crausaz99@gmail.com>
2017-09-22 14:31:22 +0200

    create JSON

--- confirm/confirm.php
+++ confirm/confirm.php
@@ -1,3 +1,16 @@
+
 <?php
     echo $_GET['mail'];
+    $JSONstring = json_encode(["mail" => $_GET['mail'], "status" => "confirmed"], JSON_PRETTY_PRINT);
+    createTempFile($JSONstring);
+
+    function createTempFile ($JSONstring) {
+        $content = file_get_contents('tmp/confirm.json');
+        $content .= $JSONstring;
+        
+        $fp = fopen('tmp/confirm.json', 'w');
+        
+        fwrite($fp, $content . ",");
+        fclose($fp);
+    }
 ?>
--- mailConfirm.php
+++ mailConfirm.php
@@ -7,8 +7,12 @@
     $message = "Bonjour," . "\n\n" .
     "Merci de confirmer votre adresse en cliquant sur le lien: " . createLink ($infos);
 
-    mail($infos['mailApp'], 'Confirmation', $message, $headers);
-
+    if (mail($infos['mailApp'], 'Confirmation', $message, $headers)) {
+        echo "mail sent";
+    }
+    else {
+        echo "error";
+    }
 
     function createLink ($infos) {
         return "http://epflform.local/confirm/confirm.php?mail=" . $infos['mailApp'];
